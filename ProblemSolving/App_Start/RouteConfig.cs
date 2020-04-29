5a5144dd9ee3717ac3ab70ab199526b8c6a630e0
Ayman <aymannaser14@gmail.com>
2020-04-29 16:59:02 +0200

    Added new problems

--- ProblemSolving/App_Start/RouteConfig.cs
+++ ProblemSolving/App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace WebApplication2
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+          
+         
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
